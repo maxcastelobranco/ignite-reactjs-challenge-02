e7394040106eb1e9ef0b09932cea88db1d1b411b
Max Lourenço Castelo Branco <max_castelo_branco@hotmail.com>
2021-03-18 21:02:31 -0300

    Finished challenge

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,9 +11,9 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export function Button({ iconName, title, selected, ...rest }: ButtonProps) {
   return (
-    <button type="button" {...(selected && { className: 'selected' })} {...rest}>
-      <Icon name={iconName} color={selected ? '#FAE800' : '#FBFBFB'} />
-      {title}
-    </button>
+      <button type="button" {...(selected && { className: 'selected' })} {...rest}>
+        <Icon name={iconName} color={selected ? '#FAE800' : '#FBFBFB'} />
+        {title}
+      </button>
   );
 }
